441ce6bc5e9948384e234bfc5762c2b22b833ab2
d231709d <sasha.sasha1002@gmail.com>
2024-10-01 22:12:51 +0300

    changes

--- internal/api/server.go
+++ internal/api/server.go
@@ -6,38 +6,71 @@ import (
 	"net/http"
 	"strconv"
 	"strings"
+	"time"
 )
 
 type Task struct {
 	ID          int
 	Title       string
 	Description string
-	Hours       int
+	Minutes     int
 	Image       string
+	Answ        string
 }
 
+type RequestTask struct {
+	TaskID     int
+	RequestID  int
+	IsRequired bool
+}
+
+type Request struct {
+	RequestTasks []RequestTask
+	DateCreate   time.Time
+	RequestGroup string
+}
+
+var tasks = []Task{
+	{ID: 1, Title: "Номер 3828 Демидович", Description: "Определить аналитическое выражение для данного интеграла, применяя методы теории интегралов.", Minutes: 10, Image: "http://127.0.0.1:9000/prog/num1.png", Answ: "http://127.0.0.1:9000/prog/3828answ.png"},
+	{ID: 2, Title: "Номер 3805 Демидович", Description: "Найти значение этого интеграла, используя знания о нормальном распределении и преобразованиях Гаусса.", Minutes: 15, Image: "http://127.0.0.1:9000/prog/num2.png", Answ: "http://127.0.0.1:9000/prog/3805answ.png"},
+	{ID: 3, Title: "Номер 3801 Демидович", Description: "Решить данный определённый интеграл, используя подходящие методики интегрирования, такие как тригонометрические подстановки или интегрирование по частям.", Minutes: 10, Image: "http://127.0.0.1:9000/prog/num3.png", Answ: "http://127.0.0.1:9000/prog/3801answ.png"},
+}
+
+var requests = []Request{
+	{
+		RequestTasks: []RequestTask{
+			{TaskID: 1, RequestID: 1, IsRequired: true},
+			{TaskID: 2, RequestID: 1, IsRequired: false},
+			{TaskID: 3, RequestID: 1, IsRequired: true},
+		},
+		DateCreate:   time.Date(2023, time.October, 3, 0, 0, 0, 0, time.UTC),
+		RequestGroup: "ИУ5-21Б",
+	},
+}
+
+func getTasksForRequest(request Request) []Task {
+	var result []Task
+	for _, reqTask := range request.RequestTasks {
+		for _, task := range tasks {
+			if task.ID == reqTask.TaskID {
+				result = append(result, task)
+			}
+		}
+	}
+	return result
+}
 func StartServer() {
 	log.Println("Server start up")
 	var specialTaskIDs = []int{1, 3} // Например, задания с ID 1 и 3
 
 	// Данные для карточек
-	tasks := []Task{
-		{ID: 1, Title: "РК 1 модуль", Description: "Рубежный контроль за первый модуль семестра 2024", Hours: 3, Image: "http://127.0.0.1:9000/prog/RK1.png"},
-		{ID: 2, Title: "РК 2 модуль", Description: "Рубежный контроль за второй модуль семестра 2024", Hours: 3, Image: "http://127.0.0.1:9000/prog/RK1.png"},
-		{ID: 3, Title: "Подготовка к экзамену", Description: "Подготовка к итоговому экзамену", Hours: 9, Image: "http://127.0.0.1:9000/prog/DZ1.png"},
-	}
+
 	r := gin.Default()
 	// Настраиваем маршрут для статических файлов
 	r.Static("/static", "./static")
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
-
-	r.GET("/home", func(c *gin.Context) {
-		searchQuery := c.Query("search")
+	r.GET("/tasks", func(c *gin.Context) {
+		searchQuery := c.Query("find_task")
 		var filteredTasks []Task
 
 		if searchQuery != "" {
@@ -50,10 +83,22 @@ func StartServer() {
 			filteredTasks = tasks
 		}
 
+		// Подсчитываем количество специальных заданий
+		specialTaskCount := 0
+		for _, task := range tasks {
+			for _, id := range specialTaskIDs {
+				if task.ID == id {
+					specialTaskCount++
+					break
+				}
+			}
+		}
+
 		c.HTML(http.StatusOK, "index.tmpl", gin.H{
-			"logo1":       "http://127.0.0.1:9000/prog/pudge.png",
-			"tasks":       filteredTasks,
-			"searchQuery": searchQuery,
+			"logo1":            "http://127.0.0.1:9000/prog/pudge.png",
+			"tasks":            filteredTasks,
+			"searchQuery":      searchQuery,
+			"SpecialTaskCount": specialTaskCount,
 		})
 	})
 	r.GET("/task/:id", func(c *gin.Context) {
@@ -69,19 +114,24 @@ func StartServer() {
 			"task": task,
 		})
 	})
-	r.GET("/special-tasks", func(c *gin.Context) {
-		var specialTasks []Task
-		for _, task := range tasks {
-			for _, id := range specialTaskIDs {
-				if task.ID == id {
-					specialTasks = append(specialTasks, task)
-					break
-				}
-			}
+	r.GET("/special-tasks/:id", func(c *gin.Context) {
+		idStr := c.Param("id")
+		id, err := strconv.Atoi(idStr)
+		if err != nil || id < 1 || id > len(tasks) {
+			c.String(http.StatusNotFound, "Задача не найдена")
+			return
 		}
-		c.HTML(http.StatusOK, "special_tasks.tmpl", gin.H{
-			"tasks": specialTasks,
-		})
+		request := requests[id-1] // Предполагаем, что у нас одна заявка для примера
+		tasksForRequest := getTasksForRequest(request)
+		data := struct {
+			Request Request
+			Tasks   []Task
+		}{
+			Request: request,
+			Tasks:   tasksForRequest,
+		}
+		log.Println(data)
+		c.HTML(http.StatusOK, "special_tasks.tmpl", data)
 	})
 
 	r.LoadHTMLGlob("templates/*")
